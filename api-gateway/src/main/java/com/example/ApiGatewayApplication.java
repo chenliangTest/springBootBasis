4fcd5a3a07a22bf67d6d86155f604999aa7fb574
chenliangtest <cheng3417885@163.com>
2019-03-27 22:24:02 +0800

    修改服务配置

--- api-gateway/src/main/java/com/example/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/ApiGatewayApplication.java
@@ -3,10 +3,12 @@ package com.example;
 import com.example.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
